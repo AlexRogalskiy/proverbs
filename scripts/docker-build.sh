de3a960eeeabbb553749c3d34490a3e866088afd
Alexander Rogalskiy <arogalskii@sbereducation.ru>
2021-08-31 13:07:12 +0300

    Updates on files
    Added changeset on workflow

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$0")" || exit 1
 
 # Build docker images
 GIT_SHA=$(git rev-parse HEAD)
-docker build -f Dockerfile -t styled-proverbs -t styled-quotes:$GIT_SHA --build-arg VERCEL_TOKEN=$1 .
+docker build -f Dockerfile -t styled-proverbs -t styled-proverbs:$GIT_SHA --build-arg VERCEL_TOKEN=$1 .
